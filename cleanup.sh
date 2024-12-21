2c452e0984a147f56b310015dbfaa0300c06470a
wnhyang <wnhyang@qq.com>
2024-12-21 11:32:20 +0800

    精简

---
+++ cleanup.sh
@@ -0,0 +1,17 @@
+git rm -r --cached .changeset
+git rm -r --cached .github
+git rm -r --cached .husky
+git rm -r --cached apps/backend-mock
+git rm -r --cached apps/web-ele
+git rm -r --cached apps/web-naive
+git rm -r --cached playground
+git rm -r --cached docs
+
+rm -rf .changeset
+rm -rf .github
+rm -rf .husky
+rm -rf apps/backend-mock
+rm -rf apps/web-ele
+rm -rf apps/web-naive
+rm -rf playground
+rm -rf docs
